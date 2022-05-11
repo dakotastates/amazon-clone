34138337d96de015800e675913d01c18b9f6e0fc
Dakota States <dakota.states@gmail.com>
2022-05-11 02:08:55 -0700

    Subtotal functionality and reducers

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,22 @@
 import '../styles/Product.css'
+import { useStateValue } from '../StateProvider'
 
 function Product({id, title, image, price, rating}) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  // console.log('This is the basket', basket)
+  const addToBasket = () =>{
+      dispatch({
+        type: 'ADD_TO_BASKET',
+        item: {
+          id: id,
+          title: title,
+          image: image,
+          price: price,
+          rating: rating
+        }
+      })
+  }
 
   return(
     <div className='product'>
@@ -23,7 +39,7 @@ function Product({id, title, image, price, rating}) {
         alt={title}
       />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
