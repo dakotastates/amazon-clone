4303d91391758da809c0e8ec17b33dfd120e2d22
Dakota States <dakota.states@gmail.com>
2022-05-11 00:47:29 -0700

    Checkout page layout, react router dom, and currency format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
+import Checkout from './components/Checkout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout/>} />
+          <Route path='/' element={<Home/>} />
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Checkout.js
@@ -0,0 +1,26 @@
+import '../styles/Checkout.css'
+import Subtotal from './Subtotal'
+
+function Checkout() {
+  return(
+    <div className='checkout'>
+      <div className='checkout__left'>
+        <img
+          className='checkout__ad'
+          src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+          alt='Ad'
+        />
+
+        <div>
+          <h2 className='checkout__title'>Shopping Cart</h2>
+          {/* BasketItems*/}
+        </div>
+      </div>
+      <div className='checkout__right'>
+        <Subtotal />
+      </div>
+    </div>
+  )
+}
+
+export default Checkout;
